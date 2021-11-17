738ddfd57a95159bd331dfba2ea304fa7a59803d
Erik znider <42530549+erikkepica@users.noreply.github.com>
2021-11-17 14:25:43 +0000

    print in slovene

--- vlaga.py
+++ vlaga.py
@@ -7,6 +7,6 @@ while True:
     humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, SHT_PIN)
     
     if humidity is not None and temperature is not None:
-        print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity))
+        print("Temperatura znasa:", temperature, "vlaga pa", humidity)
     else:
         print("Fail")
