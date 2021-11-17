c1f9d8967f878d2a35d8312e4bd9b70bd22d373f
Erik znider <42530549+erikkepica@users.noreply.github.com>
2021-11-17 14:27:20 +0000

    Fixed line 7 from SHT_PIN to DHT_PIN

--- vlaga.py
+++ vlaga.py
@@ -4,7 +4,7 @@ DHT_SENSOR = Adafruit_DHT.DHT11
 DHT_PIN = 4
 
 while True:
-    humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, SHT_PIN)
+    humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
     
     if humidity is not None and temperature is not None:
         print("Temperatura znasa:", temperature, "vlaga pa", humidity)
