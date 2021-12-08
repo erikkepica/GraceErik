7d335b0772e8be9cc598c1e493f13aa121da1e3e
Erik znider <42530549+erikkepica@users.noreply.github.com>
2021-12-08 16:18:39 +0000

    made it able to work with real information

--- send.py
+++ send.py
@@ -1,16 +1,22 @@
 import requests
+import Adafruit_DHT
 
 url="https://gracewebapp-erikznider.online404.repl.co/api/dht/"
 apikey="test"
 
-data = {
-  "temp" : "send from RPI",
-  "hum" : "send from RPI"
-}
+DHT_SENSOR = Adafruit_DHT.DHT22
+DHT_PIN = 4
 
 headers = {
     "Authorization" : "Bearer " + apikey
 }
+
+while True:
+  humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+  data = {
+    "temp" : temperature,
+    "hum" : humidity
+  }
 
 response = requests.post(url, json=data, headers=headers)
 print(response.status_code)
