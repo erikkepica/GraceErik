c46a5d02bd3c79296df16f4f9031281e79f9fc2e
Erik znider <42530549+erikkepica@users.noreply.github.com>
2021-12-15 16:09:37 +0000

    Added UsSensor

---
+++ UsSensor.py
@@ -0,0 +1,35 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+trig = 23
+echo = 24
+
+GPIO.setup(trig, GPIO.out)
+GPIO.setup(echo, GPIO.IN)
+
+def distance():
+    GPIO.output(trig, 1)
+    time.sleep(0.00001)
+    GPIO.output(trig, 0)
+
+    start_time =  time.time()
+    end_time = time.time()
+
+    while GPIO.input(echo) == 0:
+        start_time = time.time()
+    
+    while GPIO.input(echo) == 1:
+        end_time = time.time()
+
+    time_diffrance = end_time - start_time
+
+    distance = ((time_diffrance * 342)/2) * 100
+
+    return distance
+
+if __name__ == "__main__":
+    while True:
+        distance = distance()
+        print("Izmerjena razdalja je", distance, "cm")
